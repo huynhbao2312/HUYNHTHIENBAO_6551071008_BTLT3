bb6c0975967619daf0b410f7732d647de1782af5
huynhbao2312 <thienbaohuynhlop94@gmail.com>
2026-09-07 11:30:58 +0700

    Add files via upload

---
+++ BTChuong3_Bai2/BTChuong3_Bai2/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace BTChuong3_Bai2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnClickMe = new Button();
+            SuspendLayout();
+            // 
+            // btnClickMe
+            // 
+            btnClickMe.Font = new Font("Times New Roman", 44F);
+            btnClickMe.ForeColor = Color.Red;
+            btnClickMe.Location = new Point(205, 82);
+            btnClickMe.Name = "btnClickMe";
+            btnClickMe.Size = new Size(370, 258);
+            btnClickMe.TabIndex = 0;
+            btnClickMe.Text = "Click Me";
+            btnClickMe.UseVisualStyleBackColor = true;
+            btnClickMe.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnClickMe);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnClickMe;
+    }
+}
